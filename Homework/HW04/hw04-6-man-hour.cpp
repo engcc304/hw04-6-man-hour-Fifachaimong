ac425f5db893d7fec8ae849ec5bdf3a5e093de12
Fifachaimong <fifasisa36@gmail.com>
2023-08-16 14:19:27 +0700

    hw4-6

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -27,28 +27,32 @@
         Salary = U$ 374,000.00
 */
 #include <stdio.h>
-#include <locale.h>
-
+void commaprint(int number) {
+    if (number < 1000) {
+        printf("%d", number);
+        return;
+    }
+    commaprint(number / 1000);
+    printf(",%03d", number % 1000);
+}
 int main() {
-    setlocale(LC_NUMERIC, "");
-
-    char ID[10];
-    int hours;
-    int salaryperhours;
-    float Salary;
-
-    printf("Input the Employees ID:\n");
-    scanf("%s", &ID);
+    char id[10];
+    float timework;
+    float money;
+    printf("Test case: \n");
+    printf("Input the Employees ID (Max. 10 chars):\n");
+    scanf("%s", &id);
     printf("Input the working hrs:\n");
-    scanf("%d", &hours);
+    scanf("%f", &timework);
     printf("Salary amount/hr:\n");
-    scanf("%d", &salaryperhours);
-
-    Salary = salaryperhours * hours;
-
-    printf("Expected Output:\n");
-    printf("Employees ID = %s\n", ID);
-    printf("Salary = U$ %'0.2f\n", Salary);
+    scanf("%f", &money);
+    float sum;
+    sum = timework * money;
+    printf("Output: \n");
+    printf("Expected Output:\nEmployees ID = %s\n", id);
+    printf("Salary = U$ ");
+    commaprint((int)sum);
+    printf(".%02d", (int)(sum * 100) % 100);
 
     return 0;
 }
